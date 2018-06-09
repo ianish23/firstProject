f19fb681c4d5dc53ead5c324da09b5a50296007f
ianish23 <anish_git@users.noreply.github.com>
2018-06-09 21:25:49 +0530

    Added Arraylist

--- example.java
+++ example.java
@@ -4,5 +4,12 @@ import java.util.*;
 public class example{
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		int x=0;
+		List<String> aList = new ArrayList<String>();
+		aList.add("Today");
+		aList.add("New Day");
+		for(String str:aList){
+			System.out.println(str);
+		}
 	}
 }
